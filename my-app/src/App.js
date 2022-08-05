8585cb7d3c92f80d88962020de9c812cedec4679
Ethan <ethanngkindle@gmail.com>
2022-08-05 02:12:13 -0700

    Initial attempt at changing to hooks
    Didn't work, need to review documentation.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,16 +32,95 @@ function addUp(originium, orundum, tens) {
   return ten + orundums + originiums;
 }
 
-function HookCurrencyInput() {
+function HookOrundumInput() {
   const [input, setInput] = useState(() => {
     const saved = localStorage.getItem({orundum})
     const initialValue = JSON.parse(saved);
     return initialValue || "";
   });
-  
+  const handleChange = (event) => {
+    setvalue(event.target.value);
+  }
+
+  return(
+    <>
+    <fieldset>
+      <legend>Enter amount of held orundum</legend>
+      <input value={orundum}
+        onChange={this.setvalue}></input>
+    </fieldset>
+    </>
+  )
 
 }
 
+function HookOriginiumInput() {
+  const [originium, setInput] = useState(() => {
+    const saved = localStorage.getItem({originium})
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  return(
+    <>
+    <fieldset>
+      <legend>Enter amount of held orundum</legend>
+      <input value={originium}
+        onChange={this.setvalue}></input>
+    </fieldset>
+    </>
+  )
+
+}
+
+function HookTensInput() {
+  const [tens, setInput] = useState(() => {
+    const saved = localStorage.getItem({tens})
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const handleChange = (event) => {
+    setInput(event.target.value);
+    useEffect(() => {
+      // storing input name
+      localStorage.setItem(tens, JSON.stringify(value));
+    }, [tens, value]);
+  }
+
+  return(
+    <>
+    <fieldset>
+      <legend>Enter amount of held orundum</legend>
+      <input value={tens}
+        onChange={this.setvalue}></input>
+    </fieldset>
+    </>
+  )
+
+}
+
+function HookCurrencyInput(props) {
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const input = this.props.input;
+
+  return(
+    <>
+    <fieldset>
+      <legend>Enter amount of held {props.currency}</legend>
+      <input value={input}
+        onChange={this.handleChange}></input>
+    </fieldset>
+    </>
+  )
+
+}
 
 class OrundumInput extends React.Component {
   constructor(props) {
@@ -136,6 +215,86 @@ class CurrencyInput extends React.Component {
       </fieldset>
     );
   }
+}
+
+function CalculatorV2() {
+  const [tens, setTens] = useState(() => {
+    const saved = localStorage.getItem({tens})
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const [originium, setOriginium] = useState(() => {
+    const saved = localStorage.getItem({originium})
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  const [orundum, setOrundum] = useState(() => {
+    const saved = localStorage.getItem({orundum})
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+  rolls = Math.round(addUp(originium, orundum, tens) * 10)/10;
+  percentage = Math.round(rolls/3);
+
+  const handleOriginiumChange = (event) => {
+    setOriginium(event.target.value);
+    useEffect(() => {
+      // storing input name
+      localStorage.setItem(originium, JSON.stringify(event.target.value));
+    }, [originium, event.target.value]);
+  }
+
+  const handleTensChange = (event) => {
+    setTens(event.target.value);
+    useEffect(() => {
+      // storing input name
+      localStorage.setItem(tens, JSON.stringify(event.target.value));
+    }, [tens, event.target.value]);
+  }
+
+  const handleOrundumChange = (event) => {
+    setOrundum(event.target.value);
+    useEffect(() => {
+      // storing input name
+      localStorage.setItem(orundum, JSON.stringify(event.target.value));
+    }, [orundum, event.target.value]);
+  }
+
+  return(
+    <><div className="Overall-div"><div className="Left-div">
+
+
+        <HookCurrencyInput
+          currency={"orundum"}
+          input={orundum}
+          onInputChange={this.handleOrundumChange} />
+      </div><div className="Center-div">
+
+          <OriginiumInput
+            currency={"originium"}
+            input={originium}
+            onInputChange={this.handleOriginiumChange} />
+      </div><div className="Right-div">
+
+          <TensInput
+            currency={"tens"}
+            input={tens}
+            onInputChange={this.handleTensChange} />
+      </div><div className="Left-div">
+
+          <fieldset>
+            <legend>Rolls</legend>
+            <input value={rolls} />
+          </fieldset>
+      </div><div className="Center-div">
+
+          <fieldset>
+            <legend>Spark Percentage</legend>
+            <input value={percentage + '%'} />
+          </fieldset>
+        </div></div></>
+  )
+  
 }
 
 
